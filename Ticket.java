5f0ac6959580c698761509f57aff0991e0105482
imahdi2000 <imahdi@stuy.edu>
2017-04-21 11:03:18 -0400

    didn't finish ticket, will later

--- Ticket.java
+++ Ticket.java
@@ -1,3 +1,22 @@
 public class Ticket{
+
+    private int id;
+    private int vip;
+    private String name;
+    private String Summary;
+    
+    public Ticket(){
+	
+    }
+
+    public int getVip(){
+	return vip;
+    }
+        public String getSummmary(){
+	return Summary;
+    }
+        public String getname(){
+	return name;
+    }
     
 }
