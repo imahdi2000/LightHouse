669cf3fa163afde7f551ad3f71dcb356950a7ec9
imahdi2000 <imahdi@stuy.edu>
2017-04-23 22:22:03 -0400

    finished ticket

--- Ticket.java
+++ Ticket.java
@@ -1,22 +1,50 @@
+/*
+LightHouse - Ishtiaque Mahdi,Iris Tao, Angela Kim
+APCS pd 2
+Lab 3 - RUVIP?
+2017 - 04 - 23
+ */
+
+
 public class Ticket{
 
     private int id;
     private int vip;
     private String name;
-    private String Summary;
+    private String summary;
     
     public Ticket(){
-	
+	name = "John Doe";
+	vip = 0;
+	summary = "No problems, us sayin hi";
+	id = (int)(Math.random() * 10000);
+    }
+    
+    public void setVip(int num){
+	vip = num;
+    }
+    public void setSummary(String s){
+	summary = s;
+    }
+    public void setName(String s){
+	name = s;
+    }
+    public void setId(int num){
+	id = num;
     }
 
+    //-----------------------------------
     public int getVip(){
 	return vip;
     }
-        public String getSummmary(){
-	return Summary;
+    public String getSummmary(){
+	return summary;
     }
-        public String getname(){
+    public String getName(){
 	return name;
+    }
+    public int getId(){
+	return id;
     }
     
 }
